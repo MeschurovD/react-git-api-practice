b85d0d65950d6e76fe0ca51e07ede9570197ebaf
MeschurovDenis <meschurovd@gmail.com>
2021-09-07 19:06:24 +0500

    add search repositories

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { getRepos } from '../actions/repos';
@@ -8,19 +8,40 @@ import Repo from './repo/Repo';
 const Main = () => {
   const dispatch = useDispatch()
   const repos = useSelector(state => state.repos.items)
-  
+  const isFetching = useSelector(state => state.repos.isFetching)
+  const [searchValue, setSearchValue] = useState('')
+
   useEffect(() => {
-    const getR = getRepos()
-    getR(dispatch)
+    getRepos()(dispatch)
   }, [])
+  /**
+   * Изменение значения input
+   */
+  const changeInputValue = e => {
+    setSearchValue(e.target.value)
+  }
+
+  const searchRepo = () => {
+    getRepos(searchValue)(dispatch)
+    setSearchValue('')
+  }
 
   console.log(repos)
   return (
     <div>
-      <div>Main</div>
-      {repos.map(repo => {
-        return <Repo repo={repo} key={repo.id+repo.node_id}/>
-      })}
+      <div className='search'>
+        <input value={searchValue} onChange={changeInputValue} type='text' placeholder='Input repositories name' className="search__input" />
+        <button className="search__button" onClick={searchRepo}>Search</button>
+      </div>
+      {
+        isFetching
+          ?
+          <div className='fetching'></div>
+          :
+          repos.map(repo => {
+            return <Repo repo={repo} key={repo.id + repo.node_id} />
+          })
+      }
     </div>
   );
 };
--- src/reducers/reposReducer.js
+++ src/reducers/reposReducer.js
@@ -1,8 +1,9 @@
 const SET_REPOS = 'SET_REPOS'
+const SET_FETCHING = 'SET_FETCHING'
 
 const defaultState = {
   items: [{}],
-  isFetching: true,
+  isFetching: false,
 }
 
 export default function reposReducer(state = defaultState, action) {
@@ -13,8 +14,15 @@ export default function reposReducer(state = defaultState, action) {
         items: [...action.payload.items]
       }
     }
+    case SET_FETCHING: {
+      return {
+        ...state,
+        isFetching: action.payload
+      }
+    }
     default: return state
   }
 }
 
 export const setRepos = (repos) => ({type: SET_REPOS, payload: repos})
+export const setFetching = (bool) => ({type: SET_FETCHING, payload: bool})
