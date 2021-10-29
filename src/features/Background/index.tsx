bb2501b4fbf5624145f33cd82c198b5f1efdabfd
MeschurovD <meschurovd@gmail.com>
2021-10-29 17:31:39 +0500

    refactoring(layout): added background effect

---
+++ src/features/Background/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+//@ts-ignore
+import styles from './background.module.scss'
+
+const Background = () => {
+
+  const bgColor = []
+  for (let i = 1; i < 21; i++) {
+    const style = ``
+    bgColor.push(<div className={styles[`bg_color_${i}`]} />)
+  }
+  return (
+    <div className={styles.background}>
+      <div className={styles.test_1}></div>
+      <div className={styles.test_2}></div>
+    </div>
+  );
+};
+
+export default Background;
+
+//test
+// const Background = () => {
+
+//   const bgColor = []
+//   for(let i=1; i<21; i++) {
+//     const style = ``
+//     bgColor.push(<div className={styles[`bg_color_${i}`]} />)
+//   }
+//   return (
+//     <div className={styles.background}>
+//       {/* {bgColor} */}
+//       <div className={styles.test_1}></div>
+//       <div className={styles.test_2}></div>
+//       <div className={styles.test_3}></div>
+//       <div className={styles.test_4}></div>
+//       <div className={styles.test_5}></div>
+//       <div className={styles.test_6}></div>
+
+//     </div>
+//   );
+// };
